347da3e41b54514687d344ee445286e6da649ef2
jinyiwei2012 <116472418+jinyiwei2012@users.noreply.github.com>
2025-11-12 01:50:09 +0800

    Update DailyWife plugin version to v1.0.3

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class GroupMember:
 
 
 # --------------- 插件主类 ---------------
-@register("DailyWife", "jmt059", "每日老婆插件", "v1.0.2-mod", "https://github.com/jmt059/DailyWife")
+@register("DailyWife", "jmt059", "每日老婆插件", "v1.0.3", "https://github.com/jmt059/DailyWife")
 class DailyWifePlugin(Star):
     # 用于跟踪等待确认开启进阶功能的用户和会话信息
     ADVANCED_ENABLE_STATES: Dict[str, Dict[str, any]] = {}
